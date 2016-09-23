0c56d7af6afe2c7eb6b4a9aee49cde1d9ae8ab5b
Marcin Pciak <marcin.pciak@classen.com.pl>
2016-09-23 22:28:17 +0200

    init

---
+++ view/DetailInit.view.js
@@ -0,0 +1,19 @@
+﻿sap.ui.jsview("view.DetailInit", {
+	
+	getControllerName : function(){
+		return "controller.DetailInit";
+	},
+	
+	createContent : function(oController){
+		return new sap.m.FlexBox({			
+			height : "100%",
+			alignItems : sap.m.FlexAlignItems.Center,
+			justifyContent : sap.m.FlexJustifyContent.Center,
+			items : [
+				new sap.m.Image({
+					src : "img/Optima2SAP_alpha_s.png"
+				})
+			]
+		})
+	}	
+})
